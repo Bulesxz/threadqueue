903d2a7c69ae91a824dfefeb65a73c5544e52918
cout_helloworld <1252455742@qq.com>
2016-01-19 20:30:17 +0800

    Signed-off-by: cout_helloworld <1252455742@qq.com>

---
+++ base/Any.h
@@ -0,0 +1,73 @@
+#ifndef ANY_H
+#define ANY_H
+#define NULL 0
+
+
+class PlaceHolder
+{
+	public:
+		virtual ~PlaceHolder(){}
+		virtual PlaceHolder* clone() const{};
+};
+
+
+
+template<typename ValueType>
+class Holder : public PlaceHolder {
+	public:
+		Holder(const ValueType& value):held_(value) {;}
+		virtual ~Holder(){}
+		virtual PlaceHolder* clone() const 
+		{
+			  return new Holder(held_);
+		}
+	public:
+		ValueType held_;
+};
+
+
+class Any {
+	public:
+		Any() : content_(NULL) {}
+		template<typename ValueType>
+		Any(const ValueType& value) : content_(new Holder<ValueType>(value)) {
+		}
+
+		Any(const Any& other) : content_(other.content_ ? other.content_->clone() : NULL) {}
+		
+		~Any() {
+			if (content_ != NULL)
+			{
+				delete content_;
+				content_ = NULL;
+			}
+		}
+		
+		template<typename ValueType>
+		ValueType * any_cast() {
+			if (content_ == NULL)
+				return NULL;
+			else
+				return &(static_cast<Holder<ValueType> *>(content_)->held_);
+		}
+
+
+		
+	private:
+		PlaceHolder* content_;
+};
+
+/*
+template<typename ValueType>
+ValueType* any_cast() {
+		if (content == NULL)
+			return NULL;
+		else
+			return static_cast<holder<ValueType> *>(content_)->held_;
+}
+
+*/
+
+
+
+#endif 
